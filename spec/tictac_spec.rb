d4d1d60f6b4cc03914232c8cdc5c8e6a14e45969
Igors78 <oleinikovsigors@gmail.com>
2020-11-09 08:49:11 +0200

    Declaring variables for test cases

--- spec/tictac_spec.rb
+++ spec/tictac_spec.rb
@@ -9,12 +9,4 @@ describe Game do
   let(:win6) { [0, 0, 1, 0, 0, 1, 0, 0, 1] }
   let(:win7) { [0, 0, 1, 0, 1, 0, 1, 0, 0] }
   let(:win8) { [1, 0, 0, 0, 1, 0, 0, 0, 1] }
-  let(:game) { Game.new }
-
-  describe '#fill_the_board' do
-    it 'should print prompt for player 1 if it is X move' do
-      @turn = 'x'
-      expect(game.fill_the_board).to output("Player 1 (X) choice (1-9)?:").to_stdout
-    end
-  end
 end
